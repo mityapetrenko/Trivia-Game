709a3e9fb0e670c81bd43c6223d8bddad411010a
mityapetrenko <dimbaslv@gmail.com>
2019-02-21 00:00:01 -0600

    basic html and css done

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+//need a start button?? research
+// need to setup timeout for 30 sec
+// it will run a function that will let user answer questiosn
+// will need a winner/looser function
+// clear timeout????
+// reset function will clear timeout and set it to 30 seconds again. a counter will keep track of right answers/wrong 
+//answers, will need to display right answers as well.
+// done button at the end of the screen will end the game. game end screen/function will run. display counter right/wring
+//answers. unanswered questions
+//after start button is hit run function to display the questions and the timer
